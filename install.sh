6ece8f61bfde697892feea9c12e014d3ea9ca661
smck1 <s.mckeown.1@research.gla.ac.uk>
2015-07-24 14:29:48 +0100

    Fixed remnants of the previous setup, should actually work now.

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 # install.sh
 
-url='http://nlp.stanford.edu/software/stanford-ner-2015-04-20.zip'
+base_url='http://nlp.stanford.edu/software/'
+version='stanford-ner-2015-04-20'
 classifier='english.muc.7class.distsim.crf.ser.gz'
 
 npm install
-wget -O stanford-ner.zip $url
-unzip stanford-ner.zip -d /stanford-ner
+wget -O stanford-ner.zip $base_url$version.zip
+unzip stanford-ner.zip
 rm stanford-ner.zip
+mv $version stanford-ner
 
 cp stanford-ner/stanford-ner.jar stanford-ner/stanford-ner-with-classifier.jar
 
 jar -uf stanford-ner/stanford-ner-with-classifier.jar stanford-ner/classifiers/$classifier
+
+echo "Installation complete! Run run.sh to get started."
--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ nodeport='8080'
 memory='400m'
 classifier='english.muc.7class.distsim.crf.ser.gz'
 
-nohup java -mx$memory -cp stanford-ner-with-classifier.jar edu.stanford.nlp.ie.NERServer -port 9191 -loadClassifier classifiers/$classifier &> ner.out &
+nohup java -mx$memory -cp stanford-ner/stanford-ner-with-classifier.jar edu.stanford.nlp.ie.NERServer -port 9191 -loadClassifier stanford-ner/classifiers/$classifier &> ner.out &
 
-nohup node node_modules/ner-server/index.js $nodeport &> server.out &
+nohup node index.js $nodeport &> server.out &
 
 exit 0
